f2796f9aabf2382f5242f3b7e10ee698086eb615
FangedAxolotl <115540944+FangedAxolotl@users.noreply.github.com>
2023-12-23 03:01:45 +0700

    lesson8 ready :)

--- lesson8.js
+++ lesson8.js
@@ -61,3 +61,44 @@ console.log(result1) // тевирП от ycageL
 
 const result2 = spinWords( "This is a test" )
 console.log(result2) // This is a test
+
+//4
+function indexCulc(nums, target){
+    for (let i = 0; i < nums.length; i++) {
+        for(let j = 0; j < nums.length; j++) {
+            if(i === j) { 
+                continue;
+            }
+            if(nums[i] + nums[j] === target) { 
+                return [i, j];
+            }
+        }
+    }
+    return []
+}
+
+const nums = [2, 7, 11, 15]
+console.log(indexCulc(nums, 9))
+
+
+// 5
+
+function getLongestPrefix(words) {
+    let longestPrefix = "";
+    for(let i = 0; i < words[0].length; i++) {
+        let prefix = words[0][i];
+        for(let j = i + 1; j < words[0].length; j++) {
+            prefix += words[0][j];
+            if (words.every(word => word.includes(prefix)) &&
+                prefix.length > longestPrefix.length &&
+                prefix.length >= 2) {
+                longestPrefix = prefix;
+            }
+        }
+    }
+    return longestPrefix;
+}
+
+test1 = ["цветок","поток","хлопок"];
+
+console.log(getLongestPrefix(test1));
