50980772a4aef72da6d07c723d3c59ec796aee62
FangedAxolotl <115540944+FangedAxolotl@users.noreply.github.com>
2024-06-07 21:57:57 +0500

    lesson19

--- index.php
+++ index.php
@@ -1,142 +1,22 @@
-<!doctype html>
-<html lang="ru">
+<?php include 'realization.php' ?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Галлерея</title>
 </head>
 <body>
+<h1>Галерея фотографий</h1>
+<form name="upload-photo" action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="submit" name="submitPhoto" id="submit" class="submit" style="display: none"/>
+    <label class="input-file">
+        <input type="file" name="uploadPhoto" id="uploadPhoto" onchange="document.getElementById('submit').click()"/>
+    </label>
+</form>
 <?php
-    echo "<h1>Пункт 1</h1>";
-    $i = 0;
-    do{
-        if($i == 0){
-            echo '0 - это 0.'."<br>";
-        }
-
-        if($i % 2 == 0 && $i != 0){
-            echo $i.' - это чётное число.'."<br>";
-        }
-
-        if($i % 2 != 0){
-            echo $i.' - это нечётное число.'."<br>";
-        }
-        $i++;
-    }while($i < 11);
-
-    echo "<h2>Пункт 2</h2>";
-    $ranges = array(
-            "Московская область" => array("Москва", "Зеленоград", "Клин"),
-            "Ленинградская область" => array("Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"),
-            "Рязанская область" => array("Рязань", "Касимов", "Кораблино", "Спасск-Рязанский", "Скопин")
-    );
-
-    foreach ($ranges as $key => $values)
-    {
-        echo $key. ':'."<br>";
-        for($i = 0; $i < count($values) - 1; $i++){
-            echo $values[$i].', ';
-        }
-        echo $values[count($values) - 1].'.'."<br>";
-    }
-    echo "<h3>Пункт 3</h3>";
-    $dictionary = [
-        'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo',
-        'ж' => 'zh', 'з' => 'z', 'и' => 'i', 'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm',
-        'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u',
-        'ф' => 'f', 'х' => 'kh', 'ц' => 'ts', 'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sch', 'ъ' => '',
-        'ы' => 'y', 'ь' => '', 'э' => 'e', 'ю' => 'yu', 'я' => 'ya'
-    ];
-    $sentence = 'русские предложения';
-    $new_sentence = '';
-    function translate($words, $dictionary, $new_sentence)
-    {
-        $word = explode(' ', $words);
-        for ($j = 0; $j < count($word); $j++){
-            $current_word = $word[$j];
-            for ($i = 0; $i < strlen($current_word); $i++) {
-                $symbol = mb_substr($current_word, $i, 1);
-                foreach ($dictionary as $key => $value) {
-                    if($symbol == $key) {
-                        $new_sentence .= $value;
-                    }
-                }
-            }
-            $new_sentence .= ' ';
-        }
-        return $new_sentence;
-    }
-
-    echo translate($sentence, $dictionary, $new_sentence);
-    echo "<h4> Пункт 4-5</h4>";
-    $menu =  [
-        [
-            'title' => 'Машина',
-            'link' => 'car',
-            'children' => [[
-                'title' => 'Большая машина',
-                'link' => 'big-car',
-                'children' => [
-                    [
-                        'title' => 'Маленькая машина',
-                        'link' => 'small-car',
-                    ]
-                ]
-            ]],
-        ],
-        [
-            'title' => 'Окно',
-            'link' => 'window',
-            'children' => [
-                [
-                    'title' => 'Маленькое окно',
-                    'link' => 'small-window',
-                ]
-            ]
-        ],
-        [
-            'title' => 'Дверь',
-            'link' => 'door',
-        ]
-    ];
-
-    function getMenu($menu)
-    {
-        $output = '<ul style="list-style-type: square">';
-        foreach ($menu as $value) {
-            $output .= '<li>';
-            $output .= "<a href='{$value['link']}'> {$value['title']} </a>";
-            if (isset($value['children'])) {
-                $output .= getMenu($value['children']);
-            }
-            $output .= '</li>';
-        }
-        $output .= '</ul>';
-        return $output;
-    }
-    echo getMenu($menu);
-
-    echo "<h6> Пункт 6</h6>";
-
-    $ranges = array(
-        "Московская область" => array("Москва", "Зеленоград", "Клин"),
-        "Ленинградская область" => array("Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"),
-        "Рязанская область" => array("Рязань", "Касимов", "Кораблино", "Спасск-Рязанский", "Скопин")
-    );
-
-    foreach ($ranges as $key => $values)
-    {
-        echo $key. ':'."<br>";
-        $answer = array();
-        for($i = 0; $i < count($values); $i++){
-            if(mb_substr($values[$i], 0, 1) == 'К')
-                $answer[] = $values[$i];
-        }
-        echo join(', ', $answer);
-        echo '.'."<br>";
-    }
+    logQuery();
+    buildGallery("./images/");
 ?>
 </body>
 </html>
---
+++ upload.php
@@ -0,0 +1,46 @@
+<?php
+if (array_key_exists('uploadPhoto', $_FILES)) {
+    include './realization.php';
+    $target_dir = "./images/";
+    $target_file = $target_dir . basename($_FILES["uploadPhoto"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["uploadPhoto"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".<br>";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.<br>";
+            $uploadOk = 0;
+        }
+    }
+
+    if (file_exists($target_file)) {
+        echo "File already exists.<br>";
+        $uploadOk = 0;
+    }
+
+    if ($_FILES["uploadPhoto"]["size"] >= 1000000) {
+        echo "Image must be less than 1 MB.<br>";
+        $uploadOk = 0;
+    }
+
+    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "webp") {
+        echo "Only accepting JPG, PNG, JPEG and WEBP files.<br>";
+        $uploadOk = 0;
+    }
+
+    if ($uploadOk) {
+        if (move_uploaded_file($_FILES["uploadPhoto"]["tmp_name"], $target_file)) {
+            echo "The file ". htmlspecialchars( basename( $_FILES["uploadPhoto"]["name"])). " has been uploaded.";
+            transformToMiniature($target_dir, $_FILES["uploadPhoto"]["name"]);
+            header("Refresh:0; url=index.php");
+        } else {
+            echo "Sorry, there was an error uploading your file.<br>";
+        }
+    }
+}
+?>
+
+<br><a href="index.php">Return</a>
