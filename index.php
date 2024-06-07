cb76404f7956d4d3c98746de8a7d9d8289587151
FangedAxolotl <115540944+FangedAxolotl@users.noreply.github.com>
2024-06-07 18:40:53 +0500

    lesson17

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -10,111 +10,133 @@
 <body>
 <?php
     echo "<h1>Пункт 1</h1>";
-    $a = 12;
-    $b = 10;
-    if($a >= 0 && $b >= 0){
-        echo $a - $b;
-    }elseif ($a < 0 && $b < 0){
-        echo $a * $b;
-    }elseif (($a < 0 && $b >= 0) || ($a >= 0 && $b < 0)) {
-        echo $a + $b;
-    }
+    $i = 0;
+    do{
+        if($i == 0){
+            echo '0 - это 0.'."<br>";
+        }
+
+        if($i % 2 == 0 && $i != 0){
+            echo $i.' - это чётное число.'."<br>";
+        }
+
+        if($i % 2 != 0){
+            echo $i.' - это нечётное число.'."<br>";
+        }
+        $i++;
+    }while($i < 11);
 
     echo "<h2>Пункт 2</h2>";
-    $a = rand(0, 15);
-    switch ($a){
-        case 0: echo 0;
-            break;
-        case 1: echo 1;
-            break;
-        case 2: echo 2;
-            break;
-        case 3: echo 3;
-            break;
-        case 4: echo 4;
-            break;
-        case 5: echo 5;
-            break;
-        case 6: echo 6;
-            break;
-        case 7: echo 7;
-            break;
-        case 8: echo 8;
-            break;
-        case 9: echo 9;
-            break;
-        case 10: echo 10;
-            break;
-        case 11: echo 11;
-            break;
-        case 12: echo 12;
-            break;
-        case 13: echo 13;
-            break;
-        case 14: echo 14;
-            break;
-        case 15: echo 15;
-            break;
-        default: echo "Такого числа в промежутке нет!";
-    }
+    $ranges = array(
+            "Московская область" => array("Москва", "Зеленоград", "Клин"),
+            "Ленинградская область" => array("Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"),
+            "Рязанская область" => array("Рязань", "Касимов", "Кораблино", "Спасск-Рязанский", "Скопин")
+    );
 
+    foreach ($ranges as $key => $values)
+    {
+        echo $key. ':'."<br>";
+        for($i = 0; $i < count($values) - 1; $i++){
+            echo $values[$i].', ';
+        }
+        echo $values[count($values) - 1].'.'."<br>";
+    }
     echo "<h3>Пункт 3</h3>";
-    function sum($a, $b){
-        return $a + $b;
-    }
-
-    function subtract($a, $b){
-        return $a - $b;
-    }
-
-    function multiply($a, $b){
-        return $a * $b;
-    }
-
-    function divide($a, $b){
-        return $a / $b;
-    }
-
-    echo sum(2, 3);
-
-    echo "<h4> Пункт 4</h4>";
-    function mathOperation($arg1, $arg2, $operation){
-        switch ($operation){
-            case 'Sum': return sum($arg1, $arg2);
-            case 'Subtract': return subtract($arg1, $arg2);
-            case 'Multiply': return multiply($arg1, $arg2);
-            case 'Divide': return divide($arg1, $arg2);
-            default:
-                return "Operation is not exists!";
+    $dictionary = [
+        'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo',
+        'ж' => 'zh', 'з' => 'z', 'и' => 'i', 'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm',
+        'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u',
+        'ф' => 'f', 'х' => 'kh', 'ц' => 'ts', 'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sch', 'ъ' => '',
+        'ы' => 'y', 'ь' => '', 'э' => 'e', 'ю' => 'yu', 'я' => 'ya'
+    ];
+    $sentence = 'русские предложения';
+    $new_sentence = '';
+    function translate($words, $dictionary, $new_sentence)
+    {
+        $word = explode(' ', $words);
+        for ($j = 0; $j < count($word); $j++){
+            $current_word = $word[$j];
+            for ($i = 0; $i < strlen($current_word); $i++) {
+                $symbol = mb_substr($current_word, $i, 1);
+                foreach ($dictionary as $key => $value) {
+                    if($symbol == $key) {
+                        $new_sentence .= $value;
+                    }
+                }
+            }
+            $new_sentence .= ' ';
         }
+        return $new_sentence;
     }
 
-    echo mathOperation(1, 2,'Sum')."<br>";
-    echo mathOperation(1, 2,'Subtract')."<br>";
-    echo mathOperation(1, 2,'Multiply')."<br>";
-    echo mathOperation(1, 2,'Divide')."<br>";
+    echo translate($sentence, $dictionary, $new_sentence);
+    echo "<h4> Пункт 4-5</h4>";
+    $menu =  [
+        [
+            'title' => 'Машина',
+            'link' => 'car',
+            'children' => [[
+                'title' => 'Большая машина',
+                'link' => 'big-car',
+                'children' => [
+                    [
+                        'title' => 'Маленькая машина',
+                        'link' => 'small-car',
+                    ]
+                ]
+            ]],
+        ],
+        [
+            'title' => 'Окно',
+            'link' => 'window',
+            'children' => [
+                [
+                    'title' => 'Маленькое окно',
+                    'link' => 'small-window',
+                ]
+            ]
+        ],
+        [
+            'title' => 'Дверь',
+            'link' => 'door',
+        ]
+    ];
 
-    echo "<h5> Пункт 5</h5>";
-    //1
-    echo date("Y ");
-    //2
-    $example = new DateTime();
-    echo $example->format('Y ') ;
-    //3
-    echo getdate( time() )['year'];
+    function getMenu($menu)
+    {
+        $output = '<ul style="list-style-type: square">';
+        foreach ($menu as $value) {
+            $output .= '<li>';
+            $output .= "<a href='{$value['link']}'> {$value['title']} </a>";
+            if (isset($value['children'])) {
+                $output .= getMenu($value['children']);
+            }
+            $output .= '</li>';
+        }
+        $output .= '</ul>';
+        return $output;
+    }
+    echo getMenu($menu);
+
     echo "<h6> Пункт 6</h6>";
-    function power($val, $pow){
-        if($pow == 0){
-            return 1;
-        }
 
-        if ($pow == 1){
-            return $val;
+    $ranges = array(
+        "Московская область" => array("Москва", "Зеленоград", "Клин"),
+        "Ленинградская область" => array("Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"),
+        "Рязанская область" => array("Рязань", "Касимов", "Кораблино", "Спасск-Рязанский", "Скопин")
+    );
+
+    foreach ($ranges as $key => $values)
+    {
+        echo $key. ':'."<br>";
+        $answer = array();
+        for($i = 0; $i < count($values); $i++){
+            if(mb_substr($values[$i], 0, 1) == 'К')
+                $answer[] = $values[$i];
         }
-        return $val * power($val, $pow-1);
+        echo join(', ', $answer);
+        echo '.'."<br>";
     }
-
-    echo power(2, 6);
 ?>
 </body>
 </html>
